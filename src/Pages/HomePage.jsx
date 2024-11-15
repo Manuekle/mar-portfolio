090116de3d5d7970d7e7d5c838bb94d07851c83c
manuel erazo <meerazo7@hotmail.com>
2024-11-14 23:16:01 -0500

    images public

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,35 +1,42 @@
 import React from 'react';
 import Slider from '../Components/Slider.jsx';
 
+import Web1 from '../../public/Images/web.jpeg';
+import Term1 from '../../public/Images/1term.jpeg';
+import Term2 from '../../public/Images/2term.jpeg';
+import Samr from '../../public/Images/samr.jpeg';
+import Plb from '../../public/Images/plb.jpeg';
+import Poster from '../../public/Images/poster.jpeg';
+
 function HomePage() {
   const slides = [
     {
-      image: '/public/Images/1term.jpeg',
+      image: Web1,
       date: 'october 7, 2024',
       text: 'web 1.0'
     },
     {
-      image: '/public/Images/1term.jpeg',
+      image: Term1,
       date: 'september 10, 2024',
       text: '1st mid term'
     },
     {
-      image: '/public/Images/2term.jpeg',
+      image: Term2,
       date: 'october 21, 2024',
       text: '2nd mid term'
     },
     {
-      image: '/public/Images/samr.jpeg',
+      image: Samr,
       date: 'august 26, 2024',
       text: 'samr model activity'
     },
     {
-      image: '/public/Images/plb.jpeg',
+      image: Plb,
       date: 'september 02, 2024',
       text: 'plb'
     },
     {
-      image: '/public/Images/poster.jpeg',
+      image: Poster,
       date: '02 september 2024',
       text: 'poster'
     }
